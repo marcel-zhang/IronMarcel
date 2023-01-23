9b41a8b03bd6b648710ab10f14ba6feb7f230146
marcel-zhang <marcelzhang2014@gmail.com>
2023-01-23 16:27:45 +0100

    Update Lab_SQL_Intro.sql

--- Lab_SQL_Intro.sql
+++ Lab_SQL_Intro.sql
@@ -1,34 +1,33 @@
-# Explore tables by selecting all columns from the table for my client
-SELECT 
-    *
-FROM
-    sakila.customer;
+USE sakila
+
+# 2. Explore tables by selecting all columns from the table for my client
+SELECT * FROM sakila.customer;
     
-# Select one column from a table. Get film titles.
+# 3. Select one column from a table. Get film titles.
 SELECT 
     title
 FROM
     sakila.film;
     
-# Get unique list of film languages under the alias language.
+# 4. Get unique list of film languages under the alias language.
 SELECT 
     name AS language
 FROM
     sakila.language;
     
-# Find out how many stores does the company have
+# 5.1. Find out how many stores does the company have
 SELECT 
     COUNT(*)
 FROM
     sakila.store;
     
-# Find out how many employees staff does the company have
+# 5.2. Find out how many employees staff does the company have
 SELECT 
     COUNT(*)
 FROM
     sakila.staff;
     
-# Return a list of employee first names only
+# 5.3. Return a list of employee first names only
 SELECT 
     first_name
 FROM
