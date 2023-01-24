f83b0a5f8ba9e6ef4c3a37d41ae315e2ea5b22a8
marcel-zhang <marcelzhang2014@gmail.com>
2023-01-24 10:35:05 +0100

    Update Lab_SQL_Basics.sql

--- Lab_SQL_Basics.sql
+++ Lab_SQL_Basics.sql
@@ -2,201 +2,225 @@ USE bank;
 
 # Query 1: Get the id values of the first 5 clients from district_id with a value equals to 1.
 SELECT 
-    client_id
+    `client_id`
 FROM
-    bank.client
+    `client`
 WHERE
-    district_id = 1
-ORDER BY client_id
+    `district_id` = 1
+ORDER BY `client_id`
 LIMIT 5;
 
 # Query 2: In the client table, get an id value of the last client where the district_id equals to 72.
 SELECT 
-    client_id
+    `client_id`
 FROM
-    bank.client
+    `client`
 WHERE
-    district_id = 72
-ORDER BY client_id DESC
+    `district_id` = 72
+ORDER BY `client_id` DESC
 LIMIT 1;
 
 # Query 3: Get the 3 lowest amounts in the loan table.
 SELECT 
-	amount 
+	`amount` 
 FROM 
-	bank.loan
-ORDER BY amount 
+	`loan`
+ORDER BY `amount` 
 LIMIT 3;
 
 # Query 4: What are the possible values for status, ordered alphabetically in ascending order in the loan table?
 SELECT DISTINCT
-    status
+    `status`
 FROM
-    bank.loan
-ORDER BY status;
+    `loan`
+ORDER BY `status`;
 
 # Query 5: What is the loan_id of the highest payment received in the loan table?
 SELECT 
-	loan_id
+	`loan_id`
 FROM
-	bank.loan
-ORDER BY payments DESC
+	`loan`
+ORDER BY `payments` DESC
 LIMIT 1;
 
 # Query 6: What is the loan amount of the lowest 5 account_ids in the loan table? Show the account_id and the corresponding amount
 SELECT 
-    account_id, amount
+    `account_id`, `amount`
 FROM
-    bank.loan
-ORDER BY account_id
+    `loan`
+ORDER BY `account_id`
 LIMIT 5;
 
 # Query 7: What are the top 5 account_ids with the lowest loan amount that have a loan duration of 60 in the loan table?
 SELECT 
-    account_id
+    `account_id`
 FROM
-    bank.loan
+    `loan`
 WHERE
-    duration = 60
-ORDER BY amount , account_id
+    `duration` = 60
+ORDER BY `amount` , `account_id`
 LIMIT 5;
 
 # Query 8: What are the unique values of k_symbol in the order table?
 SELECT DISTINCT
-    k_symbol
+    `k_symbol`
 FROM
     `order`
-ORDER BY k_symbol;
+ORDER BY `k_symbol`;
+
+## To exclude the blank row
+SELECT DISTINCT
+    `k_symbol`
+FROM
+    `order`
+WHERE
+    `k_symbol` <> ' '
+ORDER BY `k_symbol`;
 
 # Query 9: In the order table, what are the order_ids of the client with the account_id 34?
 SELECT 
-    order_id
+    `order_id`
 FROM
     `order`
 WHERE
-    account_id = 34;
+    `account_id` = 34;
 
 # Query 10: In the order table, which account_ids were responsible for orders between order_id 29540 and order_id 29560 (inclusive)?
 SELECT DISTINCT
-    account_id
+    `account_id`
 FROM
     `order`
 WHERE
-    order_id BETWEEN 29540 AND 29560;
+    `order_id` BETWEEN 29540 AND 29560;
     
 # Query 11: In the order table, what are the individual amounts that were sent to (account_to) id 30067122?
 SELECT 
-    amount
+    `amount`
 FROM
     `order`
 WHERE
-    account_to = 30067122;
+    `account_to` = 30067122;
     
 # Query 12: In the trans table, show the trans_id, date, type and amount of the 10 first transactions from account_id 793 in chronological order, from newest to oldest.
 SELECT 
-    trans_id, date, type, amount
+    `trans_id`, `date`, `type`, `amount`
 FROM
-    trans
+    `trans`
 WHERE
-    account_id = 793
-ORDER BY date DESC
+    `account_id` = 793
+ORDER BY `date` DESC
 LIMIT 10;
+
+# OPTIONAL
+USE `bank`;
 
 # Query 13: In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? 
 # Show the results sorted by the district_id in ascending order.
 SELECT 
-    district_id, COUNT(client_id) AS client_count
+    `district_id`, COUNT(`client_id`) AS `client_count`
 FROM
-    client
+    `client`
 WHERE
-    district_id < 10
-GROUP BY district_id
-ORDER BY district_id;
+    `district_id` < 10
+GROUP BY `district_id`
+ORDER BY `district_id`;
 
 # Query 14: In the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
 SELECT 
-	type, COUNT(card_id) AS card_count
+	`type`, COUNT(`card_id`) AS `card_count`
 FROM 
-	card
-GROUP BY type
-ORDER BY card_count DESC;
+	`card`
+GROUP BY `type`
+ORDER BY `card_count` DESC;
 
 # Query 15: Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts.
 SELECT 
-    account_id, SUM(amount) AS amount_sum
+    `account_id`, SUM(`amount`) AS `amount_sum`
 FROM
-    loan
-GROUP BY account_id
-ORDER BY amount_sum DESC
+    `loan`
+GROUP BY `account_id`
+ORDER BY `amount_sum` DESC
 LIMIT 10;
 
 # Query 16: In the loan table, retrieve the number of loans issued for each day, before (excl) 930907, ordered by date in descending order.
 SELECT 
-    date, COUNT(loan_id) AS loan_count
+    `date`, COUNT(`loan_id`) AS `loan_count`
 FROM
-    loan
+    `loan`
 WHERE
-    date < 930907
-GROUP BY date
-ORDER BY date DESC;
+    `date` < 930907
+GROUP BY `date`
+ORDER BY `date` DESC;
 
 # Query 17: In the loan table, for each day in December 1997, count the number of loans issued for each unique loan duration, 
 # ordered by date and duration, both in ascending order. You can ignore days without any loans in your output.
 SELECT 
-    date, duration, COUNT(loan_id) AS loan_count
+    `date`, `duration`, COUNT(`loan_id`) AS `loan_count`
 FROM
-    loan
+    `loan`
 WHERE
-    date BETWEEN 971201 AND 971231
-GROUP BY date , duration
-ORDER BY date , duration;
+    `date` BETWEEN 971201 AND 971231
+    # `date` LIKE '9712%'
+GROUP BY `date` , `duration`
+ORDER BY `date` , `duration`;
 
-# Query 18: In the trans table, for account_id 396, sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). 
-# Your output should have the account_id, the type and the sum of amount, named as total_amount. Sort alphabetically by type.
+# Query 18: In the trans table, for account_id 396, 
+# sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). 
+# Your output should have the account_id, the type and the sum of amount, named as total_amount. 
+# Sort alphabetically by type.
 SELECT 
-    account_id, type, SUM(amount) AS total_amount
+    `account_id`, `type`, SUM(`amount`) AS `total_amount`
 FROM
-    trans
+    `trans`
 WHERE
-    account_id = 396
-GROUP BY type
-ORDER BY type;
+    `account_id` = 396
+GROUP BY `type`
+ORDER BY `type`;
 
 # Query 19: From the previous output, translate the values for type to English, 
 # rename the column to transaction_type, round total_amount down to an integer
 SELECT 
-    account_id,
+    `account_id`,
     CASE
-        WHEN type = 'VYDAJ' THEN 'OUTGOING'
-        WHEN type = 'PRIJEM' THEN 'INCOMING'
+        WHEN `type` = 'VYDAJ' THEN 'OUTGOING'
+        WHEN `type` = 'PRIJEM' THEN 'INCOMING'
     END AS 'transaction_type',
-    FLOOR(SUM(amount)) AS total_amount
+    FLOOR(SUM(`amount`)) AS `total_amount`
 FROM
-    trans
+    `trans`
 WHERE
-    account_id = 396
-GROUP BY type
-ORDER BY type;
+    `account_id` = 396
+GROUP BY `type`
+ORDER BY `type`;
 
 # Query 20: From the previous result, modify your query so that it returns only one row, 
 # with a column for incoming amount, outgoing amount and the difference.
 SELECT 
-    account_id,
-    FLOOR(SUM(CASE WHEN type = 'PRIJEM' THEN amount ELSE 0 END)) AS incoming_amount,
-    FLOOR(SUM(CASE WHEN type = 'VYDAJ' THEN amount ELSE 0 END)) AS outgoing_amount,
-    FLOOR(SUM(CASE WHEN type = 'PRIJEM' THEN amount ELSE 0 END) - SUM(CASE WHEN type = 'VYDAJ' THEN amount ELSE 0 END)) AS difference
-FROM 
-	trans
+    `account_id`,
+    FLOOR(SUM(CASE
+                WHEN `type` = 'PRIJEM' THEN `amount`
+              END)) AS `incoming_amount`,
+    FLOOR(SUM(CASE
+                WHEN `type` = 'VYDAJ' THEN `amount`
+              END)) AS `outgoing_amount`,
+    FLOOR(SUM(CASE 
+				WHEN `type` = 'PRIJEM' THEN `amount` 
+                WHEN `type` = 'VYDAJ' THEN -`amount`
+              END)) AS `difference`
+FROM
+    `trans`
 WHERE
-    account_id = 396
-GROUP BY account_id;
+    `account_id` = 396;
 
 # Query 21: Continuing with the previous example, rank the top 10 account_ids based on their difference.
 SELECT 
-    account_id,
-    FLOOR(SUM(CASE WHEN type = 'PRIJEM' THEN amount ELSE 0 END) - SUM(CASE WHEN type = 'VYDAJ' THEN amount ELSE 0 END)) AS difference
-FROM 
-	trans
-GROUP BY account_id
-ORDER BY difference DESC
+    `account_id`,
+    FLOOR(SUM(CASE 
+				WHEN `type` = 'PRIJEM' THEN `amount` 
+                WHEN `type` = 'VYDAJ' THEN - `amount`
+              END)) AS `difference`
+FROM
+    `trans`
+GROUP BY `account_id`
+ORDER BY `difference` DESC
 LIMIT 10;
